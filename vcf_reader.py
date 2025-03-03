8a4dfc5293fe6805e5b90490a9fab204c5ec9664
marti-sanchis <marti.sanchis01@estudiant.upf.edu>
2025-03-03 12:37:06 +0100

    Assign correct patient id

--- vcf_reader.py
+++ vcf_reader.py
@@ -4,19 +4,17 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from config import Config
 
-def process_vcf(vcf_file_path, patient_id=1):
+def process_vcf(vcf_file_path, patient_id):
 
     if not os.path.exists(vcf_file_path):
-        #print(f"VCF file {vcf_file_path} not found, skipping processing.")
+        print(f"VCF file {vcf_file_path} not found, skipping processing.")
         return  # Exit if no file is found
 
-    # Engine with the URI to the database
     engine = create_engine(Config.SQLALCHEMY_DATABASE_URI, echo=False)
-    # Create session to interact with database
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Check if the patient exists in the database
+    # Verificar que el patient_id es válido
     existing_patient = session.execute(
         text("SELECT 1 FROM patient WHERE patient_id = :patient_id"),
         {"patient_id": patient_id}
@@ -24,15 +22,12 @@ def process_vcf(vcf_file_path, patient_id=1):
 
     if not existing_patient:
         print(f"Error: Patient ID {patient_id} does not exist in the patient table.")
-        session.close()  # Close the session to avoid resource leakage
-        return  # Exit if the patient does not exist
+        session.close()
+        return  
 
-    # Read variant file (Variant Call Format) with pysam function.
-    # vcf_file_path = "files/AML_sample_muts.hg38.vcf"
     vcf = pysam.VariantFile(vcf_file_path)
     variants = []
 
-    # List of dictionaries: chromosome, position, reference and alternative alleles (considering multiple alt alleles). 
     for record in vcf:
         chrom = record.chrom 
         pos = record.pos  
@@ -45,7 +40,6 @@ def process_vcf(vcf_file_path, patient_id=1):
                 "alt": alt  
             })
 
-    # Search for matching variants in local DB.
     query = text("""
         SELECT variant_id FROM variant
         WHERE chromosome = :chrom
@@ -71,8 +65,7 @@ def process_vcf(vcf_file_path, patient_id=1):
         """)
         for variant_id in matched_variants:
             session.execute(insert_query, {"patient_id": patient_id, "variant_id": variant_id})
-        
-        session.commit()
-        session.close()
 
-process_vcf("uploads/AML_sample_muts.hg38.vcf")
+        session.commit()
+    
+    session.close()
